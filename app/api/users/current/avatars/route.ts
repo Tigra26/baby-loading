d3e359d7b12aa032497c70ef836d9ac8998b98b3
d-levchenko <dl.qubb@gmail.com>
2026-06-24 16:15:22 +0300

    route handlers for API; docker files for container

---
+++ app/api/users/current/avatars/route.ts
@@ -0,0 +1,33 @@
+import { logErrorResponse } from "@/app/api/_utils/utils";
+import { api } from "@/app/api/api";
+import { isAxiosError } from "axios";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+export async function PATCH(request: Request) {
+  try {
+    const cookieStore = await cookies();
+    const body = await request.json();
+
+    const res = await api.patch("/users/current/avatars", body, {
+      headers: {
+        Cookie: cookieStore.toString(),
+      },
+    });
+
+    return NextResponse.json(res.data, { status: res.status });
+  } catch (error) {
+    if (isAxiosError(error)) {
+      logErrorResponse(error.response?.data);
+      return NextResponse.json(
+        { error: error.message, response: error.response?.data },
+        { status: error.status }
+      );
+    }
+    logErrorResponse({ message: (error as Error).message });
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
