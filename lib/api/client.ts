60eeeca3935b8cad8c2f07b38fc32f2b87e36ef2
Alena <shining8smaragd@gmail.com>
2026-06-24 21:53:17 +0300

    Change base url in instance

--- lib/api/client.ts
+++ lib/api/client.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL;
-
 export const apiClient = axios.create({
-  baseURL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL + "/api",
   withCredentials: true,
 });
