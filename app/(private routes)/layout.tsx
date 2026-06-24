ed88bf82904667fcb1d4d2518e097c4703311885
Alena <shining8smaragd@gmail.com>
2026-06-24 16:06:25 +0300

    Update base project structure

--- app/(private routes)/layout.tsx
+++ app/(private routes)/layout.tsx
@@ -1,6 +1,6 @@
-import  Header  from "@/components/layout/Header/Header";
-import  SideBar  from "@/components/layout/Sidebar/Sidebar";
-import  Breadcrumbs  from "@/components/layout/Breadcrumbs/Breadcrumbs";
+import Header from "@/components/layout/Header/Header";
+import Sidebar from "@/components/layout/Sidebar/Sidebar";
+import Breadcrumbs from "@/components/layout/Breadcrumbs/Breadcrumbs";
 
 type PrivateLayoutProps = {
   children: React.ReactNode;
@@ -10,7 +10,7 @@ export default function PrivateLayout({ children }: PrivateLayoutProps) {
   return (
     <div>
       <Header />
-      <SideBar />
+      <Sidebar />
 
       <main>
         <Breadcrumbs />
