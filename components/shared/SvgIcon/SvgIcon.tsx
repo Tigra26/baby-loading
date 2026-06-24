328027723e28f675613a85629c522bf625bd8d97
Alena <shining8smaragd@gmail.com>
2026-06-24 21:23:42 +0300

    SvgIcon add icons

--- components/shared/SvgIcon/SvgIcon.tsx
+++ components/shared/SvgIcon/SvgIcon.tsx
@@ -1,13 +1,35 @@
-import HeartIcon from "@/assets/icons/heart.svg";
-import CalendarIcon from "@/assets/icons/calendar.svg";
-import UserIcon from "@/assets/icons/user.svg";
+import AccountCircleIcon from "@/assets/icons/account_circle.svg";
+import BookIcon from "@/assets/icons/book_2.svg";
+import ChairIcon from "@/assets/icons/chair.svg";
+import CloseIcon from "@/assets/icons/close.svg";
+import ConversionPathIcon from "@/assets/icons/conversion_path.svg";
+import DeleteIcon from "@/assets/icons/delete_forever.svg";
+import EditIcon from "@/assets/icons/edit_square.svg";
+import FitnessIcon from "@/assets/icons/fitness_center.svg";
+import ForkSpoonIcon from "@/assets/icons/fork_spoon.svg";
+import GoogleIcon from "@/assets/icons/google.svg";
+import ArrowDownIcon from "@/assets/icons/keyboard_arrow_down.svg";
+import ArrowUpIcon from "@/assets/icons/keyboard_arrow_up.svg";
 import LogoutIcon from "@/assets/icons/logout.svg";
+import MenuIcon from "@/assets/icons/menu.svg";
+import TodayIcon from "@/assets/icons/today.svg";
 
 const icons = {
-  heart: HeartIcon,
-  calendar: CalendarIcon,
-  user: UserIcon,
+  account: AccountCircleIcon,
+  book: BookIcon,
+  chair: ChairIcon,
+  close: CloseIcon,
+  conversion: ConversionPathIcon,
+  delete: DeleteIcon,
+  edit: EditIcon,
+  fitness: FitnessIcon,
+  food: ForkSpoonIcon,
+  google: GoogleIcon,
+  arrowDown: ArrowDownIcon,
+  arrowUp: ArrowUpIcon,
   logout: LogoutIcon,
+  menu: MenuIcon,
+  today: TodayIcon,
 };
 
 export type IconName = keyof typeof icons;
