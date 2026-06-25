4d3108cc7b7bc333e4ed809a777b683dbf679a2d
d-levchenko <dl.qubb@gmail.com>
2026-06-25 14:42:17 +0300

    register route handlers response fix

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -28,7 +28,7 @@ export async function POST(req: NextRequest) {
           cookieStore.set("refreshToken", parsed.refreshToken, options);
       }
 
-      return NextResponse.json({ success: true }, { status: apiRes.status });
+      return NextResponse.json({ status: apiRes.status });
     }
 
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
