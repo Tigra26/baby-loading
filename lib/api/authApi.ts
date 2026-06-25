41689f9a36fb468b5c05c0c992f7c91d9fef100b
Vitaliy <vitaliy19980422@gmai.com>
2026-06-25 08:17:43 +0300

    improv: added generic for api

--- lib/api/authApi.ts
+++ lib/api/authApi.ts
@@ -1,12 +1,13 @@
 import { RegisterProps, LoginProps } from "@/types/auth";
 import { apiClient } from "./client";
+import { User } from "@/types/user";
 
 export const register = async (regData: RegisterProps) => {
   const response = await apiClient.post("auth/register", regData);
-  return response;
+  return response.data;
 };
 
-export const login = async (loginData: LoginProps) => {
-  const response = await apiClient.post("/auth/login", loginData);
-  return response;
+export const login = async (loginData: LoginProps): Promise<User> => {
+  const response = await apiClient.post<User>("/auth/login", loginData);
+  return response.data;
 };
