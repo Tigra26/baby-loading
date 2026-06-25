50d14a842e92d0b12eb6bd7500abd7e6de3e97c5
Vladyslav Daletskyi <63199353+daletskyiv@users.noreply.github.com>
2026-06-26 02:02:09 +0300

    star add css

--- app/(private routes)/diary/page.tsx
+++ app/(private routes)/diary/page.tsx
@@ -1,6 +1,7 @@
 import GreetingBlock from "@/components/dashboard/GreetingBlock/GreetingBlock";
 import DiaryList from "@/components/diary/DiaryList/DiaryList";
 import { getDiaryList } from "@/lib/api/diaryApi";
+import css from "./Diary.module.css";
 
 const Diary = async () => {
   const diary = await getDiaryList();
