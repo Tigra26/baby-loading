812cc53993f45897a4483dffc29debc39c9f5759
Alena <shining8smaragd@gmail.com>
2026-06-25 20:47:29 +0300

    Add breadcrumbs component

--- components/shared/SvgIcon/SvgIcon.tsx
+++ components/shared/SvgIcon/SvgIcon.tsx
@@ -13,6 +13,7 @@ import ArrowUpIcon from "@/assets/icons/keyboard_arrow_up.svg";
 import LogoutIcon from "@/assets/icons/logout.svg";
 import MenuIcon from "@/assets/icons/menu.svg";
 import TodayIcon from "@/assets/icons/today.svg";
+import BreadcrumbsRight from "@/assets/icons/chevron_right.svg";
 
 const icons = {
   account: AccountCircleIcon,
@@ -30,6 +31,7 @@ const icons = {
   logout: LogoutIcon,
   menu: MenuIcon,
   today: TodayIcon,
+  separator: BreadcrumbsRight,
 };
 
 export type IconName = keyof typeof icons;
