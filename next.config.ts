8f36007286a91606400396293925dd5b9b26a65c
mariia <maryfurman101107@gmail.com>
2026-06-26 19:46:03 +0300

    done mob and desctop 006 007 tasks dashboard

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "lehlehka.b.goit.study",
       },
-      { protocol: "https", hostname: "ftp.goit.study" },
+      {
+        protocol: "https",
+        hostname: "ftp.goit.study",
+      },
     ],
   },
 
