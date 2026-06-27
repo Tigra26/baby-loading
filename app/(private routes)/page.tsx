e42e15d0eafd381a87e17c65703a8f43de97ebb7
d-levchenko <dl.qubb@gmail.com>
2026-06-27 13:20:38 +0300

    reminder and feelings cards for tablet and mobile

--- app/(private routes)/page.tsx
+++ app/(private routes)/page.tsx
@@ -1,10 +1,19 @@
 import TaskReminderCard from "@/components/dashboard/TasksReminderCard/TaskReminderCard";
 import DashboardClient from "./DashboardClient";
+import FeelingCheckCard from "@/components/dashboard/FeelingCheckCard/FeelingCheckCard";
+
 const HomePage = () => {
   return (
     <>
-      <DashboardClient />
-      <TaskReminderCard />
+      <div className="flex flex-col gap-4 pl-5 pr-5">
+        <DashboardClient />
+
+        <div className="flex flex-col gap-8">
+          <TaskReminderCard />
+
+          <FeelingCheckCard />
+        </div>
+      </div>
     </>
   );
 };
--- components/dashboard/FeelingCheckCard/FeelingCheckCard.tsx
+++ components/dashboard/FeelingCheckCard/FeelingCheckCard.tsx
@@ -1,5 +1,44 @@
+"use client";
+
+import AddDiaryEntryModal from "@/components/diary/AddDiaryEntryModal/AddDiaryEntryModal";
+import css from "./FeelingCheckCard.module.css";
+
+import Modal from "@/components/shared/Modal/Modal";
+import { useState } from "react";
+
 const FeelingCheckCard = () => {
-  return <div >FeelingCheckCard</div>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleModalOpen = () => {
+    setIsOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <div className={css.container}>
+      <h2 className={css.title}>Як ви себе почуваєте?</h2>
+
+      <p className={css.text}>
+        Рекомендація на сьогодні:
+        <span className={css.textSecond}>
+          Занотуйте незвичні відчуття у тілі.
+        </span>
+      </p>
+
+      <button onClick={handleModalOpen} className={css.button}>
+        Зробити запис у щоденник
+      </button>
+
+      {isOpen && (
+        <Modal onClose={handleModalClose}>
+          <AddDiaryEntryModal />
+        </Modal>
+      )}
+    </div>
+  );
 };
 
 export default FeelingCheckCard;
