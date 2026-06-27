5c49b4336ed2ea358c2c2dd6b3bdb473fc14ea27
mariia <maryfurman101107@gmail.com>
2026-06-27 11:27:06 +0300

    fixed 006 007 dashboard

--- app/(private routes)/page.tsx
+++ app/(private routes)/page.tsx
@@ -1,39 +1,9 @@
-"use client";
-
-import { useQuery } from "@tanstack/react-query";
-import { getWeeksGreeting } from "@/lib/api/dashboardApi";
-import GreetingBlock from "@/components/dashboard/GreetingBlock/GreetingBlock";
-import StatusBlock from "@/components/dashboard/StatusBlock/StatusBlock";
-import BabyTodayCard from "@/components/dashboard/BabyTodayCard/BabyTodayCard";
-import MomTipCard from "@/components/dashboard/MomTipCard/MomTipCard";
+import DashboardClient from "./DashboardClient";
 
 export default function DashboardPage() {
-  const {
-    data: weeksGreeting,
-    isLoading,
-    isError,
-  } = useQuery({
-    queryKey: ["weeksGreeting"],
-    queryFn: getWeeksGreeting,
-  });
-
   return (
     <section>
-      <GreetingBlock />
-
-      {isLoading && <p>Завантаження...</p>}
-      {isError && <p>Не вдалося завантажити дані</p>}
-
-      {weeksGreeting && (
-        <>
-          <StatusBlock
-            curWeekToPregnant={weeksGreeting.curWeekToPregnant}
-            daysBeforePregnant={weeksGreeting.daysBeforePregnant}
-          />
-          <BabyTodayCard babyToday={weeksGreeting.babyToday} />
-          <MomTipCard momHint={weeksGreeting.momHint} />
-        </>
-      )}
+      <DashboardClient />
     </section>
   );
 }
