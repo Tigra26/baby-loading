b5613b1aacc64870281761e2665f88db888ade8f
d-levchenko <dl.qubb@gmail.com>
2026-06-30 22:01:52 +0300

    fix access token updates for profile

--- components/dashboard/GreetingBlock/GreetingBlock.tsx
+++ components/dashboard/GreetingBlock/GreetingBlock.tsx
@@ -1,29 +1,16 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import css from "./GreetingBlock.module.css";
 import { useAuthStore } from "@/lib/store/authStore";
 
-const getGreeting = () => {
-  const hour = new Date().getHours();
-
-  if (hour >= 5 && hour < 12) return "Доброго ранку";
-  if (hour >= 12 && hour < 18) return "Доброго дня";
-
-  return "Доброго вечора";
-};
-
 const GreetingBlock = () => {
   const name = useAuthStore((state) => state.user?.name);
-  const [greeting, setGreeting] = useState("");
 
-  useEffect(() => {
-    setGreeting(getGreeting());
-  }, []);
+  const hour = new Date().getHours();
 
-  if (!greeting) {
-    return null;
-  }
+  let greeting = "Доброго вечора";
+  if (hour >= 5 && hour < 12) greeting = "Доброго ранку";
+  if (hour >= 12 && hour < 18) greeting = "Доброго дня";
 
   return (
     <h1 className={css.greeting}>
